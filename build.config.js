5c608d524bf1e23f0763922fe55648f6af6c40b5
Yacine Rezgui <rezgui.y@gmail.com>
2013-12-21 21:14:34 +0000

    Remove CoffeeScript directory from build config file Update vendor files

--- build.config.js
+++ build.config.js
@@ -22,9 +22,6 @@ module.exports = {
   app_files: {
     js: [ 'src/**/*.js', '!src/**/*.spec.js', '!src/assets/**/*.js' ],
     jsunit: [ 'src/**/*.spec.js' ],
-    
-    coffee: [ 'src/**/*.coffee', '!src/**/*.spec.coffee' ],
-    coffeeunit: [ 'src/**/*.spec.coffee' ],
 
     atpl: [ 'src/app/**/*.tpl.html' ],
     ctpl: [ 'src/common/**/*.tpl.html' ],
@@ -62,11 +59,13 @@ module.exports = {
    */
   vendor_files: {
     js: [
+      'vendor/lodash/lodash.js',
+      'vendor/jquery/jquery.js',
       'vendor/angular/angular.js',
-      'vendor/angular-bootstrap/ui-bootstrap-tpls.min.js',
-      'vendor/placeholders/angular-placeholders-0.0.1-SNAPSHOT.min.js',
+      'vendor/angular/angular-animate.js',
+      'vendor/angular/angular-cookies.js',
       'vendor/angular-ui-router/release/angular-ui-router.js',
-      'vendor/angular-ui-utils/modules/route/route.js'
+      'vendor/restangular/src/restangular.js',
     ],
     css: [
     ],
