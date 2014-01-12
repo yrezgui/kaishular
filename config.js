8beb387430ef45b61571bc1d7225abb91445251e
Yacine Rezgui <rezgui.y@gmail.com>
2014-01-12 16:56:47 +0000

    Update configuration file Update bower.json

--- config.js
+++ config.js
@@ -1,14 +1,41 @@
-module.exports = {
+var path    = require('path');
 
-  source: {
-    path: 'src'
-  },
+var exports = {};
 
-  dev: {
-    path: 'lib'
-  },
-
-  prod: {
-    path: 'build'
-  }
+/*
+ * Source
+ */
+exports.source = {
+  path: 'src'
 };
+
+exports.source.files = {
+  js:   path.join(exports.source.path, 'scripts/**/*.js'),
+  less: path.join(exports.source.path, 'styles/**/*.less'),
+  css:  path.join(exports.source.path, 'styles/**/*.css'),
+  jade: path.join(exports.source.path, 'views/**/*.jade'),
+  html: path.join(exports.source.path, 'views/**/*.html')
+};
+
+/*
+ * Development
+ */
+exports.dev = {
+  path: 'lib'
+};
+
+exports.source.files = {
+  js:  path.join(exports.dev.path, 'js/**/*.js'),
+  css: path.join(exports.dev.path, 'css/**/*.less'),
+  tpl: path.join(exports.dev.path, 'tpl/**/*.html')
+};
+
+/*
+ * Production
+ */
+exports.prod = {
+  path: 'build'
+};
+
+
+exports = module.exports;
